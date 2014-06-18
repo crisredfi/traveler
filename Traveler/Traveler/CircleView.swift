f23c27f47648becb040aaa01363b4e98248365d1
crisredfi <crisredfi1@gmail.com>
2014-06-18 19:05:38 +0100

    first commit

---
+++ Traveler/Traveler/CircleView.swift
@@ -0,0 +1,27 @@
+//
+//  CircleView.swift
+//  Traveler
+//
+//  Created by Crisredfi on 17/06/14.
+//  Copyright (c) 2014 crisredfi. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIView {
+
+    init(frame: CGRect) {
+        super.init(frame: frame)
+        // Initialization code
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
